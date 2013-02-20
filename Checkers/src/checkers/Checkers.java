d86195c86a826dfcb24b134f882717879f01176a
Trevon Carter <trec21@tamu.edu>
2013-02-20 13:36:34 -0600

    started checkers dev

---
+++ Checkers/src/checkers/Checkers.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkers;
+
+/**
+ *
+ * @author Trevon
+ */
+public class Checkers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
